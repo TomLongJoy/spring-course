287f0e42b71e15c6627596ba83715147222490e2
TomLongJoy <471727371@qq.com>
2023-08-29 11:17:49 +0800

    autowire - byType
    05文件实现autowire -- byType

---
+++ ch02-di-xml/src/main/java/com/bjpowernode/ba05/Student.java
@@ -0,0 +1,36 @@
+package com.bjpowernode.ba05;
+
+public class Student {
+
+    private String name;
+    private int age;
+
+    private School school;
+
+    public void setSchool(School school) {
+        this.school = school;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", school=" + school +
+                '}';
+    }
+
+
+
+
+    public void setName(String name) {
+        System.out.println("setName:"+name);
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        System.out.println("setAge:"+age);
+        this.age = age;
+    }
+
+}
