8fe673dd9bbf153dc65fa3c748f801055968dfe7
TomLongJoy <471727371@qq.com>
2023-08-29 16:54:24 +0800

    @Component 的使用
    @Repository、@Service、@Controller是给项目的对象分层的。

--- ch04-di-anno/src/main/java/com/zlj/ba01/Student.java
+++ ch04-di-anno/src/main/java/com/zlj/ba01/Student.java
@@ -10,10 +10,38 @@ import org.springframework.stereotype.Component;
  * 位置：在类的上面，
  * @Component(value = "myStudent")
  * <bean id="myStudent" class="com.zlj.ba01.Student"></bean>
+ *
+ * spring中和@Component功能一致，创建对象的注解还有：
+ * 1.@Repository（用在持久层类的上面）:放在dao的实现类上面，
+ *                                表示创建dao对象，dao对象是能访问数据库的。
+ * 2.@Service(用在业务层类的上面）：放在service的实现类上面，
+ *                              创建service对象，service对象是做业务处理，可以有事务等功能的。
+ * 3.@Controller（控制器上面）：放在控制器（处理器）类的上面，创建控制器对象的，控制器：接受用户提交数据，显示请求的处理结果。
+ *
+ * 这3个注解使用和@Component一样的，都能创建对象，但是这3个注解还有额外的功能
+ *
+ * @Repository、@Service、@Controller是给项目的对象分层的。
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
 
-@Component(value = "myStudent")
+//@Component(value = "myStudent")//1.第一种写法
+
+@Component("myStudent") //2.省略value
+
+//@Component //3.不指定对象名称，由spring提供默认名称:类名的首字母小写
 public class Student {
+
+    public Student() {
+        System.out.println("student的无参构造");
+    }
 
     private String name;
     private Integer age;
