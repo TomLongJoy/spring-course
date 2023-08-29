845586e7958769646818afac24f012b87330e2df
TomLongJoy <471727371@qq.com>
2023-08-29 11:47:57 +0800

    配置文件，多模块引入
    配置文件，多模块引入

---
+++ ch02-di-xml/src/test/java/com/zlj/MyTest06.java
@@ -0,0 +1,29 @@
+package com.zlj;
+
+
+import com.bjpowernode.ba06.Student;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest06 {
+
+
+    /**
+     * spring 默认创建对象的时间：在创建爱你spring的容器时，会创建配置文件中的所有的对象。
+     * spring 创建对象，调用的是无参构造方法
+     *
+     * */
+    @Test
+    public void test01() {
+
+        String config = "ba06/total.xml";
+        ApplicationContext ac = new ClassPathXmlApplicationContext(config);
+        //从容器中获取Student对象
+        Student myStudent = (Student) ac.getBean("myStudent");
+        System.out.println("student对象="+myStudent);
+    }
+
+
+
+}
